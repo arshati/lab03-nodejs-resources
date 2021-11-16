9a47f540761ea12ad402cfc560f855e2c2010605
arshati <am2089@hw.ac.uk>
2021-11-16 10:57:54 +0400

    updated

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express'); //creating app
+const app = express(); //make the app listen on port
+//send an HTTP response when receiving HTTP GET /
+app.get('/', (req, res) => { 
+    res.sendFile('public/index.html', { root: __dirname });
+});
+const port = process.argv[2] || process.env.PORT || 3000; const server = app.listen(port, () => {
+console.log(`Cart app listening at http://localhost:${port}`); 
+});
+
+app.use(express.static('public')); 
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname }); });
+
+//handling static HTML and EJS templates
+app.use(express.static('public')); app.set('view engine', 'ejs'); app.get('/', (req, res) => {
+    res.render('index'); //no need for ejs extension 
+});
